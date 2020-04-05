f64bbedd76456c38b83a8d99e6d436d4173fd106
Mads Bondo Dydensborg <mabd@dbc.dk>
2020-04-05 13:34:58 +0200

    Trying to center this stuff

--- html/chart.js
+++ html/chart.js
@@ -42,6 +42,7 @@ function timeSeriesChart() {
     // append the svg object to the body of the page
     // appends a 'group' element to 'svg'
     // moves the 'group' element to the top left margin
+
     var svg = d3.select("body").append("svg")
         .attr("width", width + margin.left + margin.right)
         .attr("height", height + margin.top + margin.bottom)
